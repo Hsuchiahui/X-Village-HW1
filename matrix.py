9730510f624801379af2c75b2ec3cae2bf3dc811
Hsuchiahui <a22923509@gmail.com>
2018-07-15 17:08:21 +0800

    Class HW Pratice

--- matrix.py
+++ matrix.py
@@ -7,24 +7,80 @@ class Matrix:
 
     def __init__(self, nrows, ncols):
         """Construct a (nrows X ncols) matrix"""
-        pass
+        self.matrix_list = [[random.randrange(10) for j in range(ncols)] for i in range(nrows)]
+        self.row = nrows
+        self.col = ncols
+
+
 
     def add(self, m):
         """return a new Matrix object after summation"""
-        pass
+        if self.col == m.col and self.row == m.row:
+            result_matrix = Matrix(self.row, self.col)
+            for i in range(self.row):
+                for j in range(self.col):
+                    result_matrix.matrix_list[i][j] = self.matrix_list[i][j] + m.matrix_list[i][j]
+            return result_matrix
 
     def sub(self, m):
         """return a new Matrix object after substraction"""
-        pass
+        if self.col == m.col and self.row == m.row:
+            result_matrix = Matrix(self.row, self.col)
+            for i in range(self.row):
+                for j in range(self.col):
+                    result_matrix.matrix_list[i][j] = self.matrix_list[i][j] - m.matrix_list[i][j]
+            return result_matrix
 
     def mul(self, m):
         """return a new Matrix object after multiplication"""
-        pass
-
+        if self.col == m.row :
+            result_matrix = Matrix(self.row, m.col) 
+            for i in range(self.row):
+                for j in range(m.col):
+                    tmp = 0
+                    for k in range(self.col):
+                        tmp += self.matrix_list[i][k] * m.matrix_list[k][j]
+                    result_matrix.matrix_list[i][j] = tmp
+            return result_matrix
     def transpose(self):
         """return a new Matrix object after transpose"""
-        pass
+        result_matrix = Matrix(self.row, self.col)
+        for i in range(self.row):
+            for j in range(self.col):
+                result_matrix.matrix_list[j][i] = self.matrix_list[i][j] 
+        return result_matrix
+
     
     def display(self):
         """Display the content in the matrix"""
-        pass
+        for i in range(self.row):
+            for j in range(self.col):
+                print(self.matrix_list[i][j],end=' ')
+            print()
+
+
+a_row = int(input("Enter a matrix's rows:"))
+a_col = int(input("Enter a matrix's cols:"))
+b_row = int(input("Enter a matrix's rows:"))
+b_col = int(input("Enter a matrix's cols:"))
+matrix_a = Matrix(a_row,a_col)
+matrix_b = Matrix(b_row,b_col)
+print("===== display matrix A =====")
+matrix_a.display()
+
+print("===== display matrix B =====")
+matrix_b.display()
+
+print("===== display matrix A+B =====")
+matrix_a.add(matrix_b).display()
+
+print("===== display matrix A-B =====")
+matrix_a.sub(matrix_b).display()
+
+print("===== display matrix A*B =====")
+result = matrix_a.mul(matrix_b)
+result.display()
+
+print("===== display transpose matrix A*B =====")
+result.transpose().display()
+
